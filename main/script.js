b446538e9e5af69c4fc0952717cd8fb452e3da6b
chwalsh0722 <lilspikeyc@gmail.com>
2023-07-09 15:19:44 -0500

    Commit before working on transition buttons

--- main/script.js
+++ main/script.js
@@ -8,16 +8,6 @@ function mouseoverHeaderButton(event) {
     const element = event.currentTarget;
     element.classList.add("background-purple");
 }
-// function mouseenterHeaderButton(element) {
-//     // Get button that is currently highlighted. Remove its highlight.
-//     // Add additional class for use when other button no longer hovered.
-//     const currentPageButton = document.getElementsByClassName("current-page");
-//     currentPageButton[0].classList.remove("background-purple");
-
-//     // Highlight button that is currently hovered.
-//     element.classList.add("background-purple");
-//     console.log("mouseenter complete");
-// }
 
 // Event handler for mouse leaving header button element.
 function mouseoutHeaderButton(event) {
@@ -29,35 +19,6 @@ function mouseoutHeaderButton(event) {
     const currentPageButton = document.getElementsByClassName("current-page");
     currentPageButton[0].classList.add("background-purple");
 }
-// function mouseleaveHeaderButton(element) {
-//     // Remove highlight from previously-hovered button.
-//     element.classList.remove("background-purple");
-
-//     // Get button corresponding to current webpage. Put highlight back on button
-//     const currentPageButton = document.getElementsByClassName("current-page");
-//     currentPageButton[0].classList.add("background-purple");
-//     console.log("mouseleave complete");
-// }
-
-
-// Header button elements.
-// const homeButton = document.getElementById("home-button");
-// const aboutButton = document.getElementById("about-button");
-// const projectsButton = document.getElementById("projects-button");
-// const contactButton = document.getElementById("contact-button");
-
-// // Event listeners for mouse over a header button.
-// homeButton.addEventListener("mouseenter", mouseenterHeaderButton(homeButton));
-// aboutButton.addEventListener("mouseenter", mouseenterHeaderButton(aboutButton));
-// projectsButton.addEventListener("mouseenter", mouseenterHeaderButton(projectsButton));
-// contactButton.addEventListener("mouseenter", mouseenterHeaderButton(contactButton));
-
-
-// // Event listeners for mouse leaving a header button.
-// homeButton.addEventListener("mouseleave", mouseleaveHeaderButton(homeButton));
-// aboutButton.addEventListener("mouseleave", mouseleaveHeaderButton(aboutButton));
-// projectsButton.addEventListener("mouseleave", mouseleaveHeaderButton(projectsButton));
-// contactButton.addEventListener("mouseleave", mouseleaveHeaderButton(contactButton));
 
 
 
