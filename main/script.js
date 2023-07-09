71138f4f987ceb9f137039833516d179f59fc67f
chwalsh0722 <lilspikeyc@gmail.com>
2023-07-09 18:03:01 -0500

    Added animations for buttons at top of screen

--- main/script.js
+++ main/script.js
@@ -1,27 +1,15 @@
-// Event handler for mouse entering header button element.
-function mouseoverHeaderButton(event) {
-    // Get button corresponding to current page and remove its highlight.
-    const currentPageButton = document.getElementsByClassName("current-page");
-    currentPageButton[0].classList.remove("background-purple");
-
-    // Get button that triggered event and add purple highlight.
-    const element = event.currentTarget;
-    element.classList.add("background-purple");
+// Detect transition end.
+function getTransitionEndEventName() {
+    var transitions = {
+        "transition" : "transitioned",
+        "OTransition" : "oTransitionEnd",
+        "MozTransition" : "transitioned",
+        "WebkitTransition" : "webkitTransitionEnd"
+    }
+    let bodyStyle = document.body.style;
+    for (let transition in transitions) {
+        if (bodyStyle[transition] != undefined) {
+            return transitions[transition];
+        }
+    }
 }
-
-// Event handler for mouse leaving header button element.
-function mouseoutHeaderButton(event) {
-    // Get button that triggered event and remove purple highlight.
-    const element = event.currentTarget;
-    element.classList.remove("background-purple");
-
-    // Get button corresponding to current page and add purple highlight.
-    const currentPageButton = document.getElementsByClassName("current-page");
-    currentPageButton[0].classList.add("background-purple");
-}
-
-
-
-// mouseenter - pointer moved onto element
-// mouseleave - pointer moved out of element
-// mousedown - mouse button pressed over an element
