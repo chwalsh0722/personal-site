3d652ab6066ff71729fd30c4737a153921e5152c
chwalsh0722 <lilspikeyc@gmail.com>
2023-07-11 17:34:23 -0500

    Basic logic for animating textboxes in 'About' page.

--- main/script.js
+++ main/script.js
@@ -13,3 +13,31 @@ function getTransitionEndEventName() {
         }
     }
 }
+
+function expandAboutTextWindow(event) {
+    const textboxHeader = event.srcElement;
+    const textbox = textboxHeader.parentNode;
+    textbox.classList.toggle("is-active");
+    
+}
+
+// // Expands text windows in 'About' section of website.
+// function expandAboutTextWindowEvent(element) {
+//     // Clickable element is child of a larger text container.
+//     // Get text container.
+//     const textbox = element.parentNode;
+//     // const subtext = element.nextSibling;
+//     console.log(`Element that fired event: ${textbox.id}`);
+// }
+
+// // // Adds 'click' event listener to an array of elements.
+// // function addEventListenersClick(elements, eventFunction) {
+// //     for (element in elements) {
+// //         element.addEventListener("click", eventFunction);
+// //     }
+// // }
+
+// const textboxHeaders = document.getElementsByClassName("textbox-header");
+// for (let i = 0; i < textboxHeaders.length; i++) {
+//     textboxHeaders[i].addEventListener("click", expandAboutTextWindowEvent(textboxHeaders[i]));
+// }
