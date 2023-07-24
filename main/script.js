52cc607a14755e8cbec60e91090b03dc4b969d9f
chwalsh <lilspikeyc@gmail.com>
2023-07-24 16:52:39 -0500

    Progress on adjusting how content lays itself out when window size changes for 'About' page.

--- main/script.js
+++ main/script.js
@@ -1,25 +1,44 @@
-// Detect transition end.
-function getTransitionEndEventName() {
-    var transitions = {
-        "transition" : "transitioned",
-        "OTransition" : "oTransitionEnd",
-        "MozTransition" : "transitioned",
-        "WebkitTransition" : "webkitTransitionEnd"
-    }
-    let bodyStyle = document.body.style;
-    for (let transition in transitions) {
-        if (bodyStyle[transition] != undefined) {
-            return transitions[transition];
-        }
-    }
-}
+// // Detect transition end.
+// function getTransitionEndEventName() {
+//     var transitions = {
+//         "transition" : "transitioned",
+//         "OTransition" : "oTransitionEnd",
+//         "MozTransition" : "transitioned",
+//         "WebkitTransition" : "webkitTransitionEnd"
+//     }
+//     let bodyStyle = document.body.style;
+//     for (let transition in transitions) {
+//         if (bodyStyle[transition] != undefined) {
+//             return transitions[transition];
+//         }
+//     }
+// }
 
 function expandAboutTextWindow(event) {
     const textboxHeader = event.srcElement;
     const textbox = textboxHeader.parentNode;
     const contents = textbox.children;
-    textbox.classList.toggle("is-active");
+    textbox.classList.toggle('is-active');
     for (let i = 1; i < contents.length; i++) {
-        contents[i].classList.toggle("is-active");
+        contents[i].classList.toggle('is-active');
     }
 }
+
+function getViewportWidth() {
+    return window.innerWidth;
+}
+
+function rearrangeAboutContent () {
+    console.log('resize detected');
+    const VPwidth = getViewportWidth();
+    const body = document.getElementById('body-layout-about');
+    if (VPwidth < 1300) {
+        body.style.gridTemplateColumns = '1fr';
+    } else {
+        body.style.gridTemplateColumns = 'repeat(2, 1fr)';
+    }
+}
+
+document.addEventListener('DOMContentLoaded',
+    window.addEventListener('resize', rearrangeAboutContent)
+);
